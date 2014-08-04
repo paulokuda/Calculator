282f53a99dde49d35ab88cab3c99ba152d8d62a8
Paul <paulyokuda@gmail.com>
2014-08-04 19:49:18 -0400

    initial commit

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -14,6 +14,59 @@
 
 @implementation ViewController
 
+
+-(IBAction)Number1:(id)sender{
+    
+}
+-(IBAction)Number2:(id)sender{
+    
+}
+-(IBAction)Number3:(id)sender{
+    
+}
+-(IBAction)Number4:(id)sender{
+    
+}
+-(IBAction)Number5:(id)sender{
+    
+}
+-(IBAction)Number6:(id)sender{
+    
+}
+-(IBAction)Number7:(id)sender{
+    
+}
+-(IBAction)Number8:(id)sender{
+    
+}
+-(IBAction)Number9:(id)sender{
+    
+}
+-(IBAction)Number0:(id)sender{
+    
+}
+-(IBAction)Times:(id)sender;
+{
+    
+}
+-(IBAction)Divide:(id)sender{
+    
+}
+-(IBAction)Subtract:(id)sender{
+    
+}
+-(IBAction)Plus:(id)sender{
+    
+}
+-(IBAction)Equals:(id)sender{
+    
+}
+-(IBAction)AllClear:(id)sender{
+    
+}
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
